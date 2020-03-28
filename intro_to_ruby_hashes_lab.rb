8248238c8bde1473616198d354208a3434545470
Adao Natalino <sansonov@gmail.com>
2020-03-28 16:22:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,8 +28,7 @@ end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
-  hash = {:key => }
-  return hash
+  return hash[key]
 end
 
 def update_counting_hash(hash, key)
